3d85f19c2a405d8f70f03887dd1b585fb5d58035
raulfabra <fabraraul@yahoo.com>
2023-02-23 16:02:37 +0100

    delete comments

--- project-backend-nasa/routes/news.routes.js
+++ project-backend-nasa/routes/news.routes.js
@@ -35,54 +35,77 @@ router.get("/", isLoggedIn, (req, res, next) => {
       res.render("auth/news", data);
     })
     .catch(err => next(err))
-});
+  });
+  
+  
+  /////cuando hacemos click a la noticia y se habre en una nueva pagina //////
+  router.get("/delete/:id", (req,res,next) => {
+    let id = req.params.id;
+    Comment.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect(`/news`)
 
-
-/////cuando hacemos click a la noticia y se habre en una nueva pagina //////
+    })
+    .catch((err) => console.log(err))
+  })
 
 router.get("/:date", isLoggedIn, (req, res, next) => {
   let { date } = req.params;
-  News.find({ date })
-    .populate("comments")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "author",
-        model: "User"
-      }
-    })
-    .then(result => {
-      nasaService.getNews(date)
-        .then(response => {
+  News.findOne({ date })
+  .populate("comments")
+  .populate({
+    path: "comments",
+    populate: {
+      path: "author",
+      model: "User"
+    }
+  })
+  .then(result => {
+    nasaService.getNews(date)
+    .then(response => {
+      let comments = [];
+      result.comments.forEach(comment => {
+        let commentAux = comment
+        console.log(comment)
+        if (req.session.currentUser._id == comment.author._id || req.session.currentUser.isAdmin){
+          commentAux.canDelete = true
+        }
+        comments.push(comment)
+          })
           let data = {
             news: response.data,
-            Newscomments: { result },
-            user: req.session.currentUser
-          }/* 
-          console.log("COMMMENTS: ", data.Newscomments.result[0].comments[3].author.username) */
+            Newscomments: result,
+            user: req.session.currentUser,
+          }
+          data.Newscomments.comments = comments
+          console.log("DATE: ", data.Newscomments.comments)
+          
+          /*  console.log("COMMMENTS: ", data.Newscomments.result[0].comments[3].author.username) */
           res.render("auth/newsDetail", data);
         })
-    })
-})
-
-router.post("/:date", (req, res, next) => {
-  let { date } = req.params;
-  let author = req.session.currentUser._id;
-  let { contenido } = req.body;
-
-  if (contenido) {
-    News.findOne({date})
-    .then(result => {
-      Comment.create({ contenido, author, news: result.id })
-      .then(resolve => {
-        let comments = resolve._id;
-        News.findOneAndUpdate({ date }, { $push: { comments }, author })
-          .then(result => {
-            res.redirect(`/news/${date}`)
-          })
       })
     })
+    
+    router.post("/:date", (req, res, next) => {
+      let { date } = req.params;
+      let author = req.session.currentUser._id;
+      let { contenido } = req.body;
+      
+      if (contenido) {
+        News.findOne({date})
+        .then(result => {
+          Comment.create({ contenido, author, news: result.id })
+          .then(resolve => {
+            let comments = resolve._id;
+            News.findOneAndUpdate({ date }, { $push: { comments }, author })
+            .then(result => {
+              res.redirect(`/news/${date}`)
+            })
+          })
+    })
   }
+
 })
+
 
 module.exports = router
