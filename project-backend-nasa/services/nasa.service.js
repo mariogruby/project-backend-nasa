65b9609cbd523b6f18d633168729f028541f1129
raulfabra <fabraraul@yahoo.com>
2023-03-29 13:31:57 +0200

    actualizando rama

--- project-backend-nasa/services/nasa.service.js
+++ project-backend-nasa/services/nasa.service.js
@@ -2,20 +2,15 @@ const axios = require("axios");
 
 class NasaAPI {
     constructor() {
-        this.BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=JVFT4L57e63TkJldMITNXjbzobEEy77LYApW7l9I";
+        this.BASE_URL = process.env.API_URL;
     }
     listNews() { 
-        return axios.get(`${this.BASE_URL}&start_date=2023-01-01&end_date=2023-01-12`);
+        return axios.get(`${this.BASE_URL}&start_date=2023-01-01`);
     }
     getNews(date) {
         return axios.get(`${this.BASE_URL}&date=${date}`);
     }
-    /* deleteCharacter(id) {
-        return axios.delete(`${this.BASE_URL}/${id}`);
-    }
-    createCharacter({name, occupation, debt, weapon}) {
-        return axios.post(this.BASE_URL, {name, occupation, debt, weapon});
-    } */
+   
 }
 
 module.exports = new NasaAPI();
