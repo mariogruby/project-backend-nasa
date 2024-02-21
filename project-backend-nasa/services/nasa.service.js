a964d494482102f03c6f9c96e607d9d17ebbc547
mario ruby <mariogabrielrn@gmail.com>
2024-02-21 17:01:34 +0100

    actualizada fecha de noticias en el service

--- project-backend-nasa/services/nasa.service.js
+++ project-backend-nasa/services/nasa.service.js
@@ -5,7 +5,7 @@ class NasaAPI {
         this.BASE_URL = process.env.API_URL;
     }
     listNews() { 
-        return axios.get(`${this.BASE_URL}&start_date=2023-11-05`);
+        return axios.get(`${this.BASE_URL}&start_date=2024-01-01`);
     }
     getNews(date) {
         return axios.get(`${this.BASE_URL}&date=${date}`);
